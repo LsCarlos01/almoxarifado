144be5cc80a6d7e995b54e5b04dacc95270b4f08
LsCarlos01 <lscarlosfilho@gmail.com>
2025-11-21 18:04:23 -0300

    Criação dos models dos aplicativos

--- fornecedores/models.py
+++ fornecedores/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Fornecedores(models.Model):
+    nome_empresa = models.CharField(max_length=255)
+    contato_nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    endereco = models.TextField()
+
+    def __str__(self):
+        return self.nome_empresa
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class Usuario(AbstractUser):
+    cargo = models.CharField(max_length=100, blank=True, null=True)
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.get_full_name()
